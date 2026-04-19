e58bee0bfdf7ed7691ab21d51e7c0deb69b08cbc
martin-mfg <2026226+martin-mfg@users.noreply.github.com>
2026-04-19 18:57:55 +0200

    add debug logs

--- packages/core/src/common/config.js
+++ packages/core/src/common/config.js
@@ -28,9 +28,12 @@ const parsePATsFromEnv = (env) => {
  * @returns {Record<string, string | undefined>} `process.env` if available, otherwise `{}`.
  */
 const getDefaultEnv = () => {
+  console.log((typeof process !== "undefined") + " | " + process?.env);
   if (typeof process !== "undefined" && process?.env) {
+    console.log("if branch");
     return process.env;
   }
+  console.log("else branch");
   return {};
 };
 
